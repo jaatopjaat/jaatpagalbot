d42e0e3522fc48e66b3ee439bb550054e214878c
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-09 21:53:38 +0530

    Update git.py

--- YukkiMusic/core/git.py
+++ YukkiMusic/core/git.py
@@ -46,7 +46,7 @@ def git():
         GIT_USERNAME = REPO_LINK.split("com/")[1].split("/")[0]
         TEMP_REPO = REPO_LINK.split("https://")[1]
         UPSTREAM_REPO = (
-            f"https://{GIT_USERNAME}:{config.GIT_TOKEN}@{TEMP_REPO}"
+            f"https://github.com/HYPER-AD17/DZ-MUXIC-2.0"
         )
     else:
         UPSTREAM_REPO = config.UPSTREAM_REPO
