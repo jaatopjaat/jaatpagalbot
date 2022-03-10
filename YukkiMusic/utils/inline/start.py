a7cff4a7e550330fc7f76868dd16129157b91d50
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-11 02:24:18 +0530

    hmmmmm

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -62,6 +62,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
+                text=_["S_B_5"],
+                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
+            )
+        ]
+        [
+            InlineKeyboardButton(
                 text=_["S_B_8"], callback_data="settings_back_helper"
             )
         ]
@@ -70,10 +76,10 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(
-                    text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"
+                    text=_["S_B_4"], url=f"https://t.me/SILENT_DEVS"
                 ),
                 InlineKeyboardButton(
-                    text=_["S_B_3"], url=f"{SUPPORT_GROUP}"
+                    text=_["S_B_3"], url=f"https://t.me/SILENT_BOTS"
                 ),
             ]
         )
@@ -94,20 +100,12 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     )
                 ]
             )
-    buttons.append(
-        [
-            InlineKeyboardButton(
-                text=_["S_B_5"],
-                url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
-    )
     if GITHUB_REPO and OWNER:
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
                 InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                    text=_["S_B_6"], url=f"https://github.com/HYPER-AD17"
                 ),
             ]
         )
