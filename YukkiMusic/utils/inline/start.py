3ec13b7baa653d361ed5b5752626237fa775ac95
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-11 02:42:43 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -64,13 +64,13 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
             InlineKeyboardButton(
                 text=_["S_B_5"],
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-            )
-        ]
+            ),
+        ],
         [
             InlineKeyboardButton(
                 text=_["S_B_8"], callback_data="settings_back_helper"
-            )
-        ]
+            ),
+        ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
