af74b5718c0c7ec76c3f59baa0df22857c14e475
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-03-27 23:38:35 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -71,7 +71,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
                     text=_["S_B_4"], url=f"https://t.me/SILENT_DEVS"
                 ),
                 InlineKeyboardButton(
-                    text="♡︎Assɪsᴛᴀɴᴛ", url="tg://user?id=5185393543"
+                    text="♡︎Assɪsᴛᴀɴᴛ", url="https://t.me/DevuMuxic"
                 ),
          ],
          [
